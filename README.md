f48a90d23a8248eaece17448abc4b14fff402fec
Julian Lopez <julidelva11@gmail.com>
2024-05-10 13:55:59 -0300

    ultimo commit del tp

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tp1-web2
-asdasddasasd
+Lopez Julian Agencia de jugadores Una agencia debe tener 2 tablas, los representantes disponibles y los jugadores, donde la tabla jugador va a poder tener 0 o 1 representante.
---
+++ agencia.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2024 a las 18:19:00
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `agencia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugador`
+--
+
+CREATE TABLE `jugador` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `club` text NOT NULL,
+  `representante_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `representante`
+--
+
+CREATE TABLE `representante` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `apellido` text NOT NULL,
+  `contacto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_jugador_representante` (`representante_id`);
+
+--
+-- Indices de la tabla `representante`
+--
+ALTER TABLE `representante`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `representante`
+--
+ALTER TABLE `representante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `jugador`
+--
+ALTER TABLE `jugador`
+  ADD CONSTRAINT `fk_jugador_representante` FOREIGN KEY (`representante_id`) REFERENCES `representante` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
